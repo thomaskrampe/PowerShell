68971d98962dadc1c94445f93cf73044b04c67ba
Thomas Krampe <thomas.krampe@myctx.net>
2018-07-30 16:25:40 +0200

    Initial commit

---
+++ Active Directory/GPO/GPO-BackupAndRestore.ps1
@@ -0,0 +1,64 @@
+
+<#
+ .SYNOPSIS
+        GPO-BackupAndRestore.ps1
+ .DESCRIPTION
+        Easy Script to Ex- and Import GPO's
+ .PARAMETER Mode
+        Export 
+        Import
+ .EXAMPLE
+        GPO-BackupAndRestore.ps1 -Mode Export
+        GPO-BackupAndRestore.ps1 -Mode Import
+ .LINK
+        https://github.com/thomaskrampe/
+                 
+ .NOTES
+        Author        : Thomas Krampe | t.krampe@loginconsultants.de
+        Version       : 1.0
+        Creation date : 26.07.2018 | v0.1 | Initial script
+        Last change   : 30.07.2018 | v1.0 | Release to GitHub
+#>
+cls
+
+Param(
+[Parameter(Mandatory=$True)]
+[ValidateSet(„Export“, „Import“)]
+[string]$Mode,
+ )
+ 
+ # Change the variables to your own need
+import-module grouppolicy
+$ExportFolder=“c:\_GPO-EXPORT\“
+$Importfolder=“c:\_GPO-EXPORT\“
+$Prefix=““
+$Suffix=““
+ 
+function Export-GPOs {
+    $GPO=Get-GPO –All
+    foreach ($Entry in $GPO) {
+        $Path=$ExportFolder+$entry.Displayname
+        New-Item -ItemType directory -Path $Path
+        Backup-GPO -Guid $Entry.id -Path $Path
+    }
+}
+ 
+function Import-GPOs {
+    $Folder=Get-childItem -Path $Importfolder -Exclude *.ps1
+    foreach ($Entry in $Folder) {
+        $Name=$Prefix+$Entry.Name+$Suffix
+        $Path=$Importfolder+$entry.Name
+        $ID=Get-ChildItem -Path $Path
+        New-GPO -Name $Name
+        Import-GPO -TargetName $Name -Path $Path -BackupId $ID.Name
+    }
+}
+
+ switch ($Mode){
+    {$_ -eq „Export“}
+    {Export-GPOs
+    break}
+    {$_ -eq „Import“}
+    {Import-GPOs
+    break}
+}
