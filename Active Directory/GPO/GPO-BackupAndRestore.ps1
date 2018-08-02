28bb0a56ac24c0e2e990eb74f6120c2a131a37d0
Thomas Krampe <thomas.krampe@myctx.net>
2018-08-02 14:01:59 +0200

    v. 1.1 - Minor improvements

--- Active Directory/GPO/GPO-BackupAndRestore.ps1
+++ Active Directory/GPO/GPO-BackupAndRestore.ps1
@@ -15,30 +15,38 @@
         Author        : Thomas Krampe | t.krampe@loginconsultants.de
         Version       : 1.0
         Creation date : 26.07.2018 | v0.1 | Initial script
-        Last change   : 30.07.2018 | v1.0 | Release to GitHub
+                      : 30.07.2018 | v1.0 | Release to GitHub
+        Last change   : 02.08.2018 | v1.1 | Provide Domain informations
 #>
 
 Param(
 [Parameter(Mandatory=$True)]
 [ValidateSet(„Export“, „Import“)]
-[string]$Mode,
+[string]$Mode
  )
  
  # Change the variables to your own need
 import-module grouppolicy
-$ExportFolder=“c:\_GPO-EXPORT\“
-$Importfolder=“c:\_GPO-EXPORT\“
+$ExportFolder="c:\_GPO-EXPORT\"
+$Importfolder="c:\_GPO-EXPORT\"
 
 # For importing the GPO's to a new domain you can specify either a prefix or a suffix
-$Prefix=““
-$Suffix=““
+$Prefix="New_"
+$Suffix="_001"
+
+# Domaininformation
+$QDomain = "ad1.local"
+$QDomainC = "dc01"+$QDomain
+$TDomain = "ad2.local"
+$TDOmainC = "dc01"+$TDomain
+
  
 function Export-GPOs {
     $GPO=Get-GPO –All
     foreach ($Entry in $GPO) {
         $Path=$ExportFolder+$entry.Displayname
         New-Item -ItemType directory -Path $Path
-        Backup-GPO -Guid $Entry.id -Path $Path
+        Backup-GPO -Guid $Entry.id -Path $Path -Domain $QDomain -Server $QDomainC
     }
 }
  
@@ -48,8 +56,8 @@ function Import-GPOs {
         $Name=$Prefix+$Entry.Name+$Suffix
         $Path=$Importfolder+$entry.Name
         $ID=Get-ChildItem -Path $Path
-        New-GPO -Name $Name
-        Import-GPO -TargetName $Name -Path $Path -BackupId $ID.Name
+        New-GPO -Name $Name  -Domain $TDomain -Server $TDomainC
+        Import-GPO -TargetName $Name -Path $Path -BackupId $ID.Name .$TDomain -Server $TDomainC
     }
 }
 
