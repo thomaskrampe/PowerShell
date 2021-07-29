9f108661ec46f11da1fccb537f31932cd354adce
Thomas Krampe <thomas.krampe@myctx.net>
2021-07-29 18:13:22 +0200

    test

--- terraform/Install_PS_Core.ps1
+++ terraform/Install_PS_Core.ps1
@@ -4,7 +4,9 @@
 Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-Expression
 
 # Install PowerShell core 7.1.3
-choco install powershell-core --version=7.1.3 -y
+& C:\ProgramData\chocolatey\choco.exe install powershell-core --version=7.1.3 -y
+
+Start-Sleep -Seconds 120
 
 # Set pwsh as default ssh shell
 Set-ItemProperty -Path "HKLM:\Software\OpenSSH" -Name "DefaultShell" -Value "C:\Program Files\PowerShell\7\pwsh.exe"
@@ -14,4 +16,3 @@ if (!(Test-Path -Path $PROFILE.AllUsersAllHosts)) {
     New-Item -ItemType File -Path $PSHOME\profile.ps1 -Force
     Add-Content -Path $PSHOME\Profile.ps1 -Value 'function Prompt { "PS [" + $env:COMPUTERNAME + "] " + (Get-Location) + "> " }'
   }
-  
