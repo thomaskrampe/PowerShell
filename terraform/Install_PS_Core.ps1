d6affa2de1746da3a7235d69bf229293ff091055
Thomas Krampe <thomas.krampe@myctx.net>
2021-07-29 17:55:23 +0200

    minor

--- terraform/Install_PS_Core.ps1
+++ terraform/Install_PS_Core.ps1
@@ -6,11 +6,12 @@ Invoke-WebRequest https://chocolatey.org/install.ps1 -UseBasicParsing | Invoke-E
 # Install PowerShell core 7.1.3
 choco install powershell-core --version=7.1.3 -y
 
-# Create an AllUsersCurrentHost profile
-if (!(Test-Path -Path $PROFILE.AllUsersAllHosts)) {
-    New-Item -ItemType File -Path $PSHOME\Profile.ps1 -Force
-    Add-Content -Path $PSHOME\Profile.ps1 -Value 'function Prompt { "PS [" + $env:COMPUTERNAME + "] " + (Get-Location) + "> " }'
-  }
-
 # Set pwsh as default ssh shell
 Set-ItemProperty -Path "HKLM:\Software\OpenSSH" -Name "DefaultShell" -Value "C:\Program Files\PowerShell\7\pwsh.exe"
+
+# Create an AllUsersCurrentHost profile
+if (!(Test-Path -Path $PROFILE.AllUsersAllHosts)) {
+    New-Item -ItemType File -Path $PSHOME\profile.ps1 -Force
+    Add-Content -Path $PSHOME\Profile.ps1 -Value 'function Prompt { "PS [" + $env:COMPUTERNAME + "] " + (Get-Location) + "> " }'
+  }
+  
