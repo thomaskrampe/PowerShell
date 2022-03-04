1bd15072291c55a8cc9f4ca6c743bbb2c1be728f
Thomas Krampe <thomas.krampe@myctx.net>
2022-03-04 20:07:09 +0100

    release v.0.1

--- Windows/WindowsUpdate/windows-updates.ps1
+++ Windows/WindowsUpdate/windows-updates.ps1
@@ -1,33 +1,52 @@
 ﻿# https://www.easy365manager.com/pscredential/
 # $Credential = Get-Credential
+# Output:
+# -----------------------------------------------------------------------------------------------------------------------
+# PS C:\Temp> C:\Temp\windows-updates.ps1
+# Starting Windows Updates on lic-pki-1.licdemo.local
+# Windows Malicious Software Removal Tool x64 - v5.98 (KB890830)
+# 2022-02 Cumulative Update Preview for .NET Framework 3.5, 4.7.2 and 4.8 for Windows Server 2019 for x64 (KB5011267)
+# Security Intelligence Update for Microsoft Defender Antivirus - KB2267602 (Version 1.359.1358.0)
+# 2022-02 Cumulative Update for Windows Server 2019 (1809) for x64-based Systems (KB5010351)
+# 
+# Installing 4 updates.
+# True
+# Reboot required on lic-pki-1.licdemo.local
+# -----------------------------------------------------------------------------------------------------------------------
 
-$servers = Get-Content "C:\temp\server1.txt"
-$HexPass = Get-Content "C:\temp\7sS1C5C.txt"
+$servers = Get-Content "$PSScriptRoot\server1.txt"
+$HexPass = Get-Content "$PSScriptRoot\7sS1C5C.txt"
 $Credential = New-Object -TypeName PSCredential -ArgumentList "administrator@licdemo.local", ($HexPass | ConvertTo-SecureString)
 
 ForEach ($server in $servers) {
 
     # Get available updates for target server
     write-host "Starting Windows Updates on $server"
-    $up = Invoke-Command -ComputerName $server -ScriptBlock {Start-WUScan -SearchCriteria "Type='Software' AND IsInstalled=0"} -Credential $Credential
-    $upc = $up.count
     
-    # For ... Next to write update title into log file
-    # write-host $up[0].Title
-    # write-host $up[1].Title and so on
-
+    try {
+        $up = Invoke-Command -ComputerName $server -ScriptBlock {Start-WUScan -SearchCriteria "Type='Software' AND IsInstalled=0"} -Credential $Credential
+    }
+    catch {
+        Write-Host $_.ScriptStackTrace
+    }
+    
     if ($up) { 
         # Installing updates on target server
-        write-host "Installing $upc updates." 
+        $upc = $up.count
+        write-host "Found $upc updates." 
+        
+        for ($num = 0 ; $num -le $upc ; $num++){
+            write-host "    "$up[$num].Title
+        }
+
         $cs = New-CimSession -ComputerName $server -Credential $Credential
         Install-WUUpdates -Updates $up -CimSession $cs
-        
-        
         
         $rb = Invoke-Command -ComputerName $server -ScriptBlock {Get-WUIsPendingReboot} -Credential $Credential
         
         if ($rb -eq $true) {
             write-host "Reboot required on $server"
+            # Restart-Computer -ComputerName $server -Credential $Credential -force
             }
         else { 
             write-host "No reboot required on $server"
