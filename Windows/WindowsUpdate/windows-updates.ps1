0dbfdce21542b4d3c83bcf448cd4a67eacfd649f
Thomas Krampe <thomas.krampe@myctx.net>
2022-03-06 13:59:10 +0100

    v1.1 merge

--- Windows/WindowsUpdate/windows-updates.ps1
+++ Windows/WindowsUpdate/windows-updates.ps1
@@ -46,15 +46,9 @@ param
     [Parameter (Position = 1)][Switch]$NoRestart
 )
 
-<<<<<<< HEAD
 # Read servers.txt content
 If (-not(Test-Path -Path $PSSCriptRoot\servers.txt -PathType Leaf )) {
     Write-Host "Servers file doesn't exist, exit script." -ForegroundColor Red
-=======
-# Read server.txt content
-If (-not(Test-Path -Path $PSSCriptRoot\servers.txt -PathType Leaf )) {
-    Write-Host "Servers file doesn't exist, exiting script."
->>>>>>> 4cbceb826b99b4652996634526335be1c67b0687
     exit 1
 } else {
     $servers = Get-Content "$PSScriptRoot\servers.txt"
